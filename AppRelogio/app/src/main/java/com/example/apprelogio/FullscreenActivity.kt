34826533ccf85a4ffd23bb692d69cc33364163f8
Vitor Santos <vmvs007@gmail.com>
2026-03-31 16:33:29 +0100

    AppRelogio

---
+++ AppRelogio/app/src/main/java/com/example/apprelogio/FullscreenActivity.kt
@@ -0,0 +1,82 @@
+package com.example.apprelogio
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.apprelogio.databinding.ActivityFullscreenBinding
+
+
+class FullscreenActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFullscreenBinding.inflate(layoutInflater)
+    }
+
+    private var isChecked=true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        }
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        binding.layoutMenu.animate().translationY(500F)
+
+        val bateriaReceiver : BroadcastReceiver=object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+
+                if(intent!=null){
+                    val nivel : Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0)
+                    binding.textNivelBateria.text="$nivel %"
+                }
+            }
+        }
+
+        registerReceiver(bateriaReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+
+        binding.checkNivelBateria.setOnClickListener {
+            if(isChecked){
+                isChecked=false
+                binding.checkNivelBateria.isChecked=false
+                binding.textNivelBateria.visibility=View.GONE
+            }else{
+                isChecked=true
+                binding.checkNivelBateria.isChecked=true
+                binding.textNivelBateria.visibility=View.VISIBLE
+
+            }
+        }
+
+        binding.imageViewPreferencias.setOnClickListener {it:View->
+            binding.layoutMenu.visibility=View.VISIBLE
+            binding.layoutMenu.animate().translationY(0F).setDuration(
+                resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
+            )
+
+        }
+
+        binding.imageViewFechar.setOnClickListener {
+
+            binding.layoutMenu.animate().translationY(binding.layoutMenu.measuredHeight.toFloat()).setDuration(
+                resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
+            )
+            
+        }
+
+    }
+
+}
